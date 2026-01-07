afa670cdcdd7007a3f737f088dcbff9f60a7be34
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-01-07 11:01:41 +0500

    Task 3.1) Registratioon Form in Frontend (API)

--- Greeting.jsx
+++ Greeting.jsx
@@ -1,27 +1,32 @@
 import { useState } from "react";
 
 export default function Greeting() {
-  const [message, setMessage] = useState(0);
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const increment = async () => {
-    const res = await fetch("http://localhost:5000/count");
-    const data = await res.json();
-    setMessage(data.count);
-  };
-
-  const decrement = async () => {
-    const res = await fetch("http://localhost:5000/discount");
-    const data = await res.json();
-    setMessage(data.count);
-  };
-
+const reg=async()=>{
+  const res=await fetch("http://localhost:5000/form",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({name,email,password})
+  })
+  const data=await res.json();
+ setMessage(data.msg);
+}
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{message}</h2>
+      <input type="text" placeholder="Name"  name="name" onChange={(e)=>setName(e.target.value)}/>
+      <input type="email" placeholder="Email"name="email" onChange={(e)=>setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" name="password" onChange={(e)=>setPassword(e.target.value)} />
 
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <button onClick={reg}>Registration</button>
+<p>{message}</p>
     </div>
   );
 }
