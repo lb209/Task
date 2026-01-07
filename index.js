d4c63f3f635e8538b2a37b4f4a2d6f8c802ea20b
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-01-07 11:02:48 +0500

    Task 3.1) Registration Form in Backend

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-let count = 0; // 🔑 counter stored here
+// submit form
+let user=[]
+app.post("/form",async(req,res)=>{
+  const {name,email,password}=req.body;
+  if(!name||!email||!password){
+    return res.status(400).json({msg:"Please enter all fields"});
+  }
+  const userexist= await user.find(user=>user.email===email);
+  if(userexist){
+    return res.status(400).json({msg:"User already exists"});
+  }
+  user.push({name,email,password});
+  res.status(200).json({msg:"Registration successful"});
+})
 
-// increment
-app.get("/count", (req, res) => {
-  count++;
-  res.json({ count });
-});
-
-// decrement
-app.get("/discount", (req, res) => {
-  count--;
-  res.json({ count });
-});
 
 app.listen(5000, () => {
   console.log("Backend running on http://localhost:5000");
